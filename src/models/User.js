b71a2ba42932510419e1ac929ce316e296bd0152
mahmedzero110-sudo <mahmedzero110@gmail.com>
2026-02-03 21:36:19 +0200

    إصلاح تشفير كلمات السر ومشاكل Vercel

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const db = require('../database/db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 class User {
     static async create({ name, email, password, phone, role = 'customer', google_id = null }) {
